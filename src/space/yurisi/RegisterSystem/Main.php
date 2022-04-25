17d833e11530138d2da976ee9558051f602ff8b5
yurisi0212 <dev.yurisi@gmail.com>
2022-04-25 11:20:06 +0900

    少し進めた

--- src/space/yurisi/RegisterSystem/Main.php
+++ src/space/yurisi/RegisterSystem/Main.php
@@ -4,17 +4,17 @@ declare(strict_types=1);
 namespace space\yurisi\RegisterSystem;
 
 use pocketmine\plugin\PluginBase;
-use space\yurisi\RegisterSystem\database\PlayerData;
+use space\yurisi\RegisterSystem\database\sqlite;
 use space\yurisi\RegisterSystem\event\JoinEvent;
 use space\yurisi\RegisterSystem\event\LoginEvent;
 
 class Main extends PluginBase {
 
-	private PlayerData $data;
+	private sqlite $data;
 
     protected function onEnable(): void {
 		$this->loadEvents();
-		$this->data = new PlayerData($this);
+		$this->data = new sqlite($this);
     }
 
 	private function loadEvents(){
@@ -22,7 +22,7 @@ class Main extends PluginBase {
 		$this->getServer()->getPluginManager()->registerEvents(new LoginEvent(), $this);
 	}
 
-	public function getPlayerData(): PlayerData {
+	public function getPlayerData(): sqlite {
 		return $this->data;
 	}
 
